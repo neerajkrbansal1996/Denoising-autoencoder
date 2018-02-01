7a7ee133eb051e3f63752176eb628838ab3187be
Neeraj <neerajkrbansal1996@gmail.com>
2018-02-02 02:22:54 +0530

    Updating model

---
+++ test_model.py
@@ -0,0 +1,56 @@
+import numpy as np
+from keras.models import Model
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+from keras.models import load_model
+from sklearn.metrics import label_ranking_average_precision_score
+import time
+
+print('Loading mnist dataset')
+t0 = time.time()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))  # adapt this if using `channels_first` image data format
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `channels_first` image data format
+
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
+
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+t1 = time.time()
+print('mnist dataset loaded in: ', t1-t0)
+
+print('Loading model :')
+t0 = time.time()
+autoencoder = load_model('autoencoder.h5')
+encoder = Model(inputs=autoencoder.input, outputs=autoencoder.get_layer('encoder').output)
+t1 = time.time()
+print('Model loaded in: ', t1-t0)
+
+scores = []
+
+def plot_denoised_images():
+    n = 10
+    denoised_images = autoencoder.predict(x_test_noisy.reshape(x_test_noisy.shape[0], x_test_noisy.shape[1], x_test_noisy.shape[2], 1))
+    plt.figure(figsize = (20,4))
+    for i in range(n):
+        #Display original
+        ax = plt.subplot(2, n, i+1)
+        plt.imshow(x_test_noisy[i].reshape(28,28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        #Display Reconstruction
+        ax = plt.subplot(2, n, i+1+n)
+        plt.imshow(denoised_images[i].reshape(28,28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
+
+# To plot a denoised image
+plot_denoised_images()
